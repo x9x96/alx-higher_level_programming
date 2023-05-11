4442838f65051a9a5615e221c8794bb62e677834
x9x96 <israelsbook@gmail.com>
2023-05-11 14:45:03 -0700

    fix bugs

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
-
 def magic_calculation(a, b):
-    """Match bytecode provided by Holberton School."""
     from magic_calculation_102 import add, sub
 
     if a < b:
         x = add(a, b)
         for evry in range(4, 6):
-            x = add(x, i)
+            x = add(x, evry)
         return (x)
 
     else:
