0cbc6f342f2148e828157b75d954d130600966ed
x9x96 <israelsbook@gmail.com>
2023-05-15 07:57:23 -0700

    fixin bugs #3

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,39 +1,33 @@
 #include "lists.h"
+
 /**
- * is_palindrome - prints all elements of listint_t list
- * @head: head pointer
- * Return: nodes
+ * palindrom - prints all elements of listint_t list
+ * @head: head
+ * Return: 0 (not a palindrome), 1 (a palindrome)
  */
 int is_palindrome(listint_t **head)
 {
-	listint_t *temp = *head, *temp2 = *head;
-	int array[5000];
-	int len, j, i;
+	if (head == NULL || *head == NULL)
+		return (1);
+	return (palind_checker(head, *head));
+}
 
-	if (!head)
-	{
-		return (0);
-	}
-	if (!*head || ((*head)->next == NULL))
+/**
+ * palind_checker - checks for palindrome
+ * @head: head list
+ * @end: end list
+ */
+int palind_checker(listint_t **head, listint_t *end)
+{
+	if (!end)
 	{
 		return (1);
 	}
-	for (len = 0; temp2->next != NULL; len++)
+	if (palind_checker(head, end->next) && (*head)->n == end->n)
 	{
-		temp2 = temp2->next;
+		*head = (*head)->next;
+		return (1);
 	}
-	for (i = 0; i <= len; i++)
-	{
-		array[i] = temp->n;
-		temp = temp->next;
-	}
-
-	for (j = 0, len; j < len; j++, len--)
-	{
-		if (array[j] != array[len])
-		{
-			return (0);
-		}
-	}
-	return (1);
+	return (0);
 }
+
--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -5,16 +5,15 @@
 #include <stdlib.h>
 #include <string.h>
 
-
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
 int is_palindrome(listint_t **head);
-
+int palind_checker(listint_t **head, listint_t *end);
 #endif /* LISTS_H */
