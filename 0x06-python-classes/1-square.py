2c269523373430da32cec8ae14b762cacc65eb50
x9x96 <israelsbook@gmail.com>
2023-05-30 07:57:18 -0700

    fixing 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 """declearing the function"""
 
+
 class Square:
     """blah"""
+
 
     def __init__(self, size):
         """Initialize a new Square.
         Args:
             size (int): The size of the new square.
         """
-	    self._size = size
+        self._size = size
