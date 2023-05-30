f2e409525a19b3fabc92a2801c13c33764ecf2e6
x9x96 <israelsbook@gmail.com>
2023-05-30 07:33:17 -0700

    fixed 0

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -2,6 +2,6 @@
 """oops seems like docstring comments are now mandatory here"""
 
 
-class Square:
-	"""and apparently here as well"""
+class Square:	
+    """and apparently here as well"""
     pass
