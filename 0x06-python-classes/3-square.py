a41a3448dc954270196b7072c19e636c0c5fefd6
x9x96 <israelsbook@gmail.com>
2023-05-30 09:06:50 -0700

    mass test

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -7,11 +7,15 @@ class Square:
     __size = None
 
     def __init__(self, size=0):
-        """Instantiation with optional size"""
+        """Instation with optional size"""
         if type(size) is int:
             if size >= 0:
                 self.__size = size
             else:
-                raise ValueError("size must be >= 0")
+                raise ValueError("Size must be >= 0")
         else:
             raise TypeError("size must be an integer")
+
+    def area(self):
+        """Public instance method area"""
+        return self.__size * self.__size
