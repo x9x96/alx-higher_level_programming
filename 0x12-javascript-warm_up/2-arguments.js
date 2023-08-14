e1eb7e8f713796bdcd5593dfd14918068e3b7947
x9x96 <israelsbook@gmail.com>
2023-08-14 16:50:00 -0700

    fix-in bugs

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
-const numArgs = process.argv.length - 2;
-if (numArgs === 0) {
+if (process.argv.length === 2) {
   console.log('No argument');
-} else if (numArgs === 1) {
+} else if (process.argv.length === 3) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
